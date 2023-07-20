fb65996b754e3669654ca698156bd41924dff38c
deborahzellweger <128902123+deborahzellweger@users.noreply.github.com>
2023-07-20 10:59:39 +0100

    changed img names

---
+++ styles/portfolio.js
@@ -0,0 +1,21 @@
+for (let el of document.querySelectorAll('.hover')) {
+  let image = new Image();
+  image.img = el.dataset.img;
+  console.log(el.dataset)
+  image.className = "followMouse";
+  el.addEventListener('mouseover',(e)=>{
+    document.getElementById('container').append(image);
+    image.style.left = `${e.x - (image.width/2)}px`;
+    image.style.top = `${e.y - (image.height/2)}px`;
+  })
+  el.addEventListener('mouseout',(e)=>{
+    image.remove();
+  })
+}
+window.addEventListener('mousemove',(e)=> {
+  let image = document.querySelector('.followMouse');
+  if (image) {
+    image.style.left = `${e.x - (image.width/2)}px`;
+    image.style.top = `${e.y - (image.height/2)}px`;
+  }
+})
