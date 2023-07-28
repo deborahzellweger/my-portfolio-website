10f98cf7f2a578bba21edccb03e807dbdd402f42
deborahzellweger <128902123+deborahzellweger@users.noreply.github.com>
2023-07-28 11:50:46 +0100

    added project reveal (whoop)

--- styles/portfolio.js
+++ styles/portfolio.js
@@ -1,14 +1,21 @@
 for (let el of document.querySelectorAll('.p-hp-hover')) {
 let image = new Image();
-console.log(image);
 image.src = el.dataset.src;
-console.log(image.src)
+image.className = "followMouse"
 el.addEventListener('mouseover', (e)=> {
   document.getElementById('project-reveal').append(image);
-  image.style.top = "200px";
-  image.style.left = "300px";
+  image.style.left = `${e.x - (image.width/2)}px`;
+  image.style.top = `${e.y - (image.height/2)}px`;
 })
 el.addEventListener('mouseout',(e)=>{
     image.remove();
   })
 }
+
+window.addEventListener('mousemove',(e)=> {
+  let image = document.querySelector('.followMouse');
+  if (image) {
+    image.style.left = `${e.x - (image.width/2)}px`;
+    image.style.top = `${e.y - (image.height/2)}px`;
+  }
+})
