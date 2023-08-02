7a45b30bb8c3e2cdd40ba9b202551c9c62ada2de
deborahzellweger <128902123+deborahzellweger@users.noreply.github.com>
2023-08-02 19:53:47 +0100

    formatted liquid bodies page

--- styles/portfolio.js
+++ styles/portfolio.js
@@ -1,21 +1,21 @@
-for (let el of document.querySelectorAll('.p-hp-hover')) {
-let image = new Image();
-image.src = el.dataset.src;
-image.className = "followMouse"
-el.addEventListener('mouseover', (e)=> {
-  document.getElementById('project-reveal').append(image);
-  image.style.left = `${e.x - (image.width/2)}px`;
-  image.style.top = `${e.y - (image.height/2)}px`;
-})
-el.addEventListener('mouseout',(e)=>{
+for (let el of document.querySelectorAll(".p-hp-hover")) {
+  let image = new Image();
+  image.src = el.dataset.src;
+  image.className = "followMouse";
+  el.addEventListener("mouseover", (e) => {
+    document.getElementById("project-reveal").append(image);
+    image.style.left = `${e.x - image.width / 1}px`;
+    image.style.top = `${e.y - image.height / 1}px`;
+  });
+  el.addEventListener("mouseout", (e) => {
     image.remove();
-  })
+  });
 }
 
-window.addEventListener('mousemove',(e)=> {
-  let image = document.querySelector('.followMouse');
+window.addEventListener("mousemove", (e) => {
+  let image = document.querySelector(".followMouse");
   if (image) {
-    image.style.left = `${e.x - (image.width/2)}px`;
-    image.style.top = `${e.y - (image.height/2)}px`;
+    image.style.left = `${e.x - image.width / 1}px`;
+    image.style.top = `${e.y - image.height / 1}px`;
   }
-})
+});
